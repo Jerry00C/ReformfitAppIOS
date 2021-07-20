ccd08e2207c5ee07ed6a85464448f50f5194e1aa
Jerry <jiangshan1030@gmail.com>
2021-07-19 22:14:00 -0400

    Initial Commit

---
+++ ReformFIT/ReformFITApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReformFITApp.swift
+//  ReformFIT
+//
+//  Created by J on 2021-07-19.
+//
+
+import SwiftUI
+
+@main
+struct ReformFITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
